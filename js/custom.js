6086fd8677073a18b2621b1909e16baf1df2e740
Paulo Roberto <psface1@gmail.com>
2016-02-24 21:31:35 -0300

    First commit, organization of files,nav and header added, start of search page

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  'use strict';
+
+  let defaultBg = 'url(../../imgs/bg1.jpg)';
+  const searchClose = () => {
+    $('section#search').hide();
+    $('header, nav').show();
+  };
+
+  $('a#search_link').click(()=>{
+    $('section#search').show();
+    $('header, nav').hide();
+  });
+
+  $('span#close').click(searchClose);
+  $('body').keydown((event) => {
+      if(event.which == 27){
+        searchClose();
+      }
+  });
+
+  setInterval(() =>{
+    $('header').addClass("transitionBG");
+    if(defaultBg ==  'url(../../imgs/bg1.jpg)'){
+      defaultBg = "url(../../imgs/bg2.jpg)";
+      $('header').css("background-image", "url(../../imgs/bg2.jpg)");
+    } else if(defaultBg ==  'url(../../imgs/bg2.jpg)'){
+      defaultBg = "url(../../imgs/bg3.jpg)";
+      $('header').css("background-image", "url(../../imgs/bg3.jpg)");
+    }else{
+      defaultBg = "url(../../imgs/bg1.jpg)";
+      $('header').css("background-image", "url(../../imgs/bg1.jpg)");
+    }
+  }, 3000) ;
+
+});
